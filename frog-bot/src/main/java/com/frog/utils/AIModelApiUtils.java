3c6d277a9d441da8f7708d16cd9526f444489294
小气大方的阿俊 <130641570+851543@users.noreply.github.com>
2025-02-19 14:57:28 +0800

    新增ai接口

--- frog-bot/src/main/java/com/frog/utils/AIModelApiUtils.java
+++ frog-bot/src/main/java/com/frog/utils/AIModelApiUtils.java
@@ -8,6 +8,8 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.methods.CloseableHttpResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,6 +49,35 @@ public class AIModelApiUtils {
         try {
             paramMap.put("prompt",prompt);
             Map options = new HashMap<String,Object>();
+            paramMap.put("options",options);
+            options.put("temperature",0.2);
+            options.put("top_p",0.7);
+            options.put("repeat_penalty",1.2);
+            options.put("mirostat_tau",3.0);
+            paramMap.put("options",options);
+            return HttpClientUtil.doPost4JsonStream(URL, paramMap);
+        } catch (IOException e) {
+            log.error(e);
+        }
+        return null;
+    }
+
+    /**
+     * 聊天流式
+     *
+     * @return
+     */
+    public static CloseableHttpResponse chatStream(String prompt) {
+        try {
+            ArrayList<Object> messages = new ArrayList<Object>();
+            Map info = new HashMap<String, String>();
+            info.put("system ", "你的名字为朱斌博老师,你是一名农业专家,你可以回答各种农业问题。");
+            info.put("role", "user");
+            info.put("content", prompt);
+            messages.add(info);
+            paramMap.put("messages", messages);
+            Map options = new HashMap<String,Object>();
+            paramMap.put("options",options);
             options.put("temperature",0.2);
             options.put("top_p",0.7);
             options.put("repeat_penalty",1.2);
